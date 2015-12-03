99b60aec9fc70445477b263be26535368c605c0d
pzhaoyang <pzhaoyang@126.com>
2015-12-03 23:46:14 +0800

    Inital SmartPlug

---
+++ src/com/smartonecorner/smartplug/Login.java
@@ -0,0 +1,20 @@
+package com.smartonecorner.smartplug;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class Login extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+	}
+	
+
+}
